134a8bc8782a2bed169b3090ef4dc95b8c5aedf4
Ivan Stokolias <111705853+IvanStokolias@users.noreply.github.com>
2023-07-26 11:23:53 -0400

    Update database_connection_issue.py

--- database_connection_issue.py
+++ database_connection_issue.py
@@ -1,1 +1,14 @@
+import random
+
+def connect_to_database():
+    if random.choice([True, False]):
+        raise ConnectionError("Failed to connect to database.")
+    else:
+        print("Connected to database successfully.")
+
+if __name__ == "__main__":
+    try:
+        connect_to_database()
+    except ConnectionError as e:
+        print(e)
 
